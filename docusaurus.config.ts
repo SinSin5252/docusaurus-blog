e5e866d62943be7e92b8806553c668f5fd753e33
SinSin5252 <saglam.sinan52@gmail.com>
2026-08-03 12:16:06 +0200

    update onBrokenMarkdownLinks and some changes in blog.md

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,7 +30,12 @@ const config: Config = {
   deploymentBranch: process.env.DEPLOYMENT_BRANCH,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown:{
+    hooks:{
+      onBrokenMarkdownLinks: 'warn',
+    }
+  },
+  
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
